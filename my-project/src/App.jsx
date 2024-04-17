cb14ff9121df2500be21320fa0caad7e06e74cdd
vishal27shetty <vishal27shetty@gmail.com>
2024-04-17 22:38:58 +0530

    pg connected

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,8 +1,28 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import Login from "./Login.jsx"
 export default function App() {
+  const [backendData, setBackeddata] =  useState([{}])
+  
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+          setBackeddata(data)
+      }
+    
+    )
+  }, [])
   return (
     <div>
+      <div>
+        {(typeof backendData.users === 'undefined') ? (
+          <p>Loading...</p>): (
+            backendData.users.map((users,i) => {
+              <p key={i}>{user}</p>
+            })
+        )}
+      </div>
     <Login />
     </div>
   )
